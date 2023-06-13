ac7f840a1f2b6ea623792c707926da47ae264fca
isagrt <isadoragoedert05@gmail.com>
2023-06-13 15:12:39 -0300

    importação stack navigator

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialIcons } from '@expo/vector-icons';
